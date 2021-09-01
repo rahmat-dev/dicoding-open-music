00e27d58fcdd1ec46498a5c76c903cc43112547c
Rahmat Sulistio <rr7465457@gmail.com>
2021-09-01 23:46:09 +0700

    s3 storage implemented

---
+++ src/api/uploads/routes.js
@@ -0,0 +1,17 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/upload/pictures',
+    handler: handler.postUploadImageHandler,
+    options: {
+      payload: {
+        allow: 'multipart/form-data',
+        multipart: true,
+        output: 'stream',
+        maxBytes: 500 * 1024, // 500KB
+      },
+    },
+  },
+];
+
+module.exports = routes;
